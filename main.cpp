1e7292a8ed76ada37432be025f1c3ada438c6c82
anggabard <angel.ardeleanu@hotmail.com>
2017-01-08 01:24:29 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,6 +11,13 @@ void frana_frame(Uint32 moment_zero)
 		SDL_Delay((1000 / fps) - (SDL_GetTicks() - moment_zero));
 }
 
+void adauga_img_aici(SDL_Surface * img, int x, int y, SDL_Surface * dest)
+{
+	img->clip_rect.x = x;
+	img->clip_rect.y = y;
+	SDL_BlitSurface(img, NULL, dest, &img->clip_rect);
+}
+
 class Sprite
 {
 private:
@@ -54,11 +61,17 @@ int main(int argc, char* args[])
 	SDL_Surface *ecran = SDL_GetWindowSurface(fereastra);
 	Uint32 negru = SDL_MapRGB(ecran->format, 0,0,0);
 	Uint32 alb = SDL_MapRGB(ecran->format, 255, 255, 255);
-	SDL_FillRect(ecran, NULL, negru);
 	
-	
-	Sprite dreptunghi(alb, w/4, h/4, 200, 200);
-	dreptunghi.deseneaza(ecran);
+	SDL_Surface *BG = SDL_LoadBMP("BG.bmp");
+	SDL_Rect coordonateBG = BG->clip_rect;
+	SDL_BlitSurface(BG, NULL, ecran, &coordonateBG);
+
+	SDL_Surface *Joc_nou = SDL_LoadBMP("joc_nou.bmp");
+	adauga_img_aici(Joc_nou, latime_ecran / 2 - Joc_nou->clip_rect.w / 2, lungime_ecran / 2 - Joc_nou->clip_rect.h / 2 - 100, ecran);
+
+	SDL_Surface *iesire = SDL_LoadBMP("iesire.bmp");
+	adauga_img_aici(iesire, latime_ecran / 2 - iesire->clip_rect.w / 2, lungime_ecran / 2 - iesire->clip_rect.h / 2 + 100, ecran);
+
 	
 	SDL_UpdateWindowSurface(fereastra);
 	
@@ -76,9 +89,11 @@ int main(int argc, char* args[])
 				merge = false;
 				break;
 			}
-			cout << "ceapa";
-			SDL_UpdateWindowSurface(fereastra);
+			
 		}
+		
+		
+		SDL_UpdateWindowSurface(fereastra);
 		frana_frame(moment_zero);
 	}
 
