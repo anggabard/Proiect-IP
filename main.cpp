7d0208fd28a6387df1b0a8fa04a9e002d035a1cd
TeodorPloae <ploaetdr@yahoo.com>
2017-01-11 02:09:05 +0200

    Teo's Patch

--- main.cpp
+++ main.cpp
@@ -18,6 +18,13 @@ void adauga_img_aici(SDL_Surface * img, int x, int y, SDL_Surface * dest)
 	SDL_BlitSurface(img, NULL, dest, &img->clip_rect);
 }
 
+bool este_deasupra(SDL_Rect zona, int x, int y)
+{
+	if (x > zona.x && x < zona.x + zona.w && y > zona.y && y < zona.y + zona.h)
+		return true;
+	return false;
+}
+
 class Sprite
 {
 private:
@@ -41,7 +48,12 @@ public:
 	}
 };
 
+struct hexagon
+{
+	int val;
 
+
+}hex[19];
 int main(int argc, char* args[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -52,7 +64,7 @@ int main(int argc, char* args[])
 		SDL_WINDOWPOS_UNDEFINED,
 		1280,
 		1024,
-		SDL_WINDOW_FULLSCREEN_DESKTOP);
+		SDL_WINDOW_FULLSCREEN);
 
 	int latime_ecran;
 	int lungime_ecran;
@@ -67,10 +79,9 @@ int main(int argc, char* args[])
 	SDL_BlitSurface(BG, NULL, ecran, &coordonateBG);
 
 	SDL_Surface *Joc_nou = SDL_LoadBMP("joc_nou.bmp");
-	adauga_img_aici(Joc_nou, latime_ecran / 2 - Joc_nou->clip_rect.w / 2, lungime_ecran / 2 - Joc_nou->clip_rect.h / 2 - 100, ecran);
 
 	SDL_Surface *iesire = SDL_LoadBMP("iesire.bmp");
-	adauga_img_aici(iesire, latime_ecran / 2 - iesire->clip_rect.w / 2, lungime_ecran / 2 - iesire->clip_rect.h / 2 + 100, ecran);
+
 
 	
 	SDL_UpdateWindowSurface(fereastra);
@@ -78,25 +89,134 @@ int main(int argc, char* args[])
 	SDL_Event event;
 	bool merge = true;
 	Uint32 moment_zero;
-
+	bool joc_nou_apasat = false;
 	while (merge)
 	{
 		moment_zero = SDL_GetTicks();
 		while (SDL_PollEvent(&event))
 		{
-			if (event.type == SDL_QUIT)
+			switch (event.type)
 			{
-				merge = false;
+			case SDL_KEYDOWN:
+			{
+								switch (event.key.keysym.sym)
+								{
+								case SDLK_ESCAPE:
+								{
+													merge = false;
+													break;
+								}
+								}
+								break;
+
+			}
+			case SDL_MOUSEMOTION:
+			{
+										int x = NULL, y = NULL;
+										SDL_GetMouseState(&x , &y);
+										switch (este_deasupra(Joc_nou->clip_rect, x, y))
+										{
+											case true:
+											{
+												SDL_Surface *Joc_nou_selected = SDL_LoadBMP("joc_nou_selected.bmp");
+												adauga_img_aici(Joc_nou_selected, latime_ecran / 2 - Joc_nou_selected->clip_rect.w / 2, lungime_ecran / 2 - Joc_nou_selected->clip_rect.h / 2 - 200, ecran);
+												SDL_UpdateWindowSurface(fereastra);
+														 break;
+
+
+											}
+											case false:
+											{
+														  switch (este_deasupra(iesire->clip_rect, x, y))
+														  {
+														  case true:
+														  {
+																	   SDL_Surface *iesire_selected = SDL_LoadBMP("iesire_selected.bmp");
+																	   adauga_img_aici(iesire_selected, latime_ecran / 2 - iesire_selected->clip_rect.w / 2, lungime_ecran / 2 - iesire_selected->clip_rect.h / 2 + 200, ecran);
+																	   SDL_UpdateWindowSurface(fereastra);
+																	   break;
+														  }
+														  case false:
+														  {
+																		SDL_BlitSurface(BG, NULL, ecran, &coordonateBG);
+																		adauga_img_aici(Joc_nou, latime_ecran / 2 - Joc_nou->clip_rect.w / 2, lungime_ecran / 2 - Joc_nou->clip_rect.h / 2 - 200, ecran);
+																		adauga_img_aici(iesire, latime_ecran / 2 - iesire->clip_rect.w / 2, lungime_ecran / 2 - iesire->clip_rect.h / 2 + 200, ecran);
+																		SDL_UpdateWindowSurface(fereastra);
+																		break;
+														  }
+														  default:
+															  break;
+														  }
+											}
+											default:
+												break;
+										}
+										break;
+
+
+			}
+			case SDL_MOUSEBUTTONDOWN:
+			{
+						int x = NULL, y = NULL;
+						SDL_GetMouseState(&x, &y);
+						if (este_deasupra(iesire->clip_rect, x, y))
+							merge = false;
+						if (este_deasupra(Joc_nou->clip_rect, x, y))
+						{	
+							SDL_Surface * fundal = SDL_LoadBMP("Fundal.bmp");
+							SDL_FreeSurface(ecran);
+							SDL_BlitSurface(fundal, NULL, ecran, &fundal->clip_rect);
+							SDL_UpdateWindowSurface(fereastra);
+
+							joc_nou_apasat = true;
+							merge = false;
+						}
+						break;
+			}
+			default:
 				break;
 			}
+
 			
-		}
-		
-		
+		}	
 		SDL_UpdateWindowSurface(fereastra);
 		frana_frame(moment_zero);
 	}
+	
+	if (joc_nou_apasat)
+	{
+		bool ma_joc = true;
+		SDL_Event event_joc;
+			while (ma_joc)
+				{
+				while (SDL_PollEvent(&event_joc))
+						{
+								
+							switch (event_joc.type)
+									{
+										case SDL_KEYDOWN:
+										{
+											switch (event_joc.key.keysym.sym)
+											{
+												case SDLK_ESCAPE:
+												{
+													ma_joc = false;
+													SDL_DestroyWindow(fereastra);
+													SDL_Quit();
+													break;
+												}
+												
 
+												default:
+												break;
+											}
+										}
+
+			 						}
+						}
+				}
+	}
+	
 	SDL_DestroyWindow(fereastra);
 
 	SDL_Quit();
